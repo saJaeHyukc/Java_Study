f2661a9b4fc9b3cbf1f598a3c15c2aa520d9d200
saJaeHyukc <wogur981208@gmail.com>
2023-06-10 03:09:13 +0900

    git init

---
+++ src/ch01/HelloJavaJava.java
@@ -0,0 +1,10 @@
+package ch01;
+
+public class HelloJavaJava {
+  public static void main(String[] args) {
+    System.out.println("Hello, Java");
+  }
+}
+// 패키지 안에 자바 클래스를 포함하는 것이 좋음
+// 패키지 이름은 소문자로 구성
+// 자바 클래스 파일 안에 클래스가 여러개 들어갈 수 있는데 public이라는 클래스는 하나만 있어야함
